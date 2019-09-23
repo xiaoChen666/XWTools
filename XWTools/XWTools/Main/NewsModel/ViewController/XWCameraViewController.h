47180a8cea1c2e9ac23c4c46aeccfe40e3bace10
xiaoChen666 <xuwen_chens@163.com>
2019-09-23 09:12:48 +0800

    1 小修改

---
+++ XWTools/XWTools/Main/NewsModel/ViewController/XWCameraViewController.h
@@ -0,0 +1,17 @@
+//
+//  XWCameraViewController.h
+//  XWTools
+//
+//  Created by xuwen_chen on 2019/9/23.
+//  Copyright © 2019 mac. All rights reserved.
+//
+
+#import "CXWBaseViewController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface XWCameraViewController : CXWBaseViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ XWTools/XWTools/Main/NewsModel/ViewController/XWCameraViewController.m
@@ -0,0 +1,32 @@
+//
+//  XWCameraViewController.m
+//  XWTools
+//
+//  Created by xuwen_chen on 2019/9/23.
+//  Copyright © 2019 mac. All rights reserved.
+//
+
+#import "XWCameraViewController.h"
+
+@interface XWCameraViewController ()
+
+@end
+
+@implementation XWCameraViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
